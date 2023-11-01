4beafbb98827a73a7e86f3e3f42c9b04e2ecc129
soy6032 <404444@naver.com>
2023-11-01 15:31:44 +0900

    first commit

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -11,7 +11,7 @@ const FullLayout = lazy(() => import("../layouts/FullLayout"));
  const Join = lazy(() => import("../views/JoinForm"))
  const Info = lazy(() => import("../views/Info"));
 
-/*****Routes******/
+/*****Routes*****dd*/
 
 const ThemeRoutes = [
   {
